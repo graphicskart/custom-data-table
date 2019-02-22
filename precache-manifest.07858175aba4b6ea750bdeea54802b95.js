aba22f0a3979d9c22477131d682b4e0fe60e8b4b
Rupinderthind <rupinderpalthind@gmail.com>
2019-02-22 23:53:55 +0530

    Updates

---
+++ precache-manifest.07858175aba4b6ea750bdeea54802b95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65201e530eb5930cc3c",
+    "url": "/custom-data-table/static/js/runtime~main.a65201e5.js"
+  },
+  {
+    "revision": "d053afeaf3e07adf1df3",
+    "url": "/custom-data-table/static/js/main.d053afea.chunk.js"
+  },
+  {
+    "revision": "69f6dcd161ac31e73f56",
+    "url": "/custom-data-table/static/js/2.69f6dcd1.chunk.js"
+  },
+  {
+    "revision": "d053afeaf3e07adf1df3",
+    "url": "/custom-data-table/static/css/main.5cf18468.chunk.css"
+  },
+  {
+    "revision": "aec784e0f91f5cd3f5b03513b4bc4a6f",
+    "url": "/custom-data-table/index.html"
+  }
+];
